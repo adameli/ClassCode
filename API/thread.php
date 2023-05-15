cb230421ac58b093850f10f2fa038c3185ba7fc0
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-05-15 14:07:37 +0100

    Fixed thread.php POST to get a specific thread.

--- API/thread.php
+++ API/thread.php
@@ -13,51 +13,73 @@
     // Create thread
     if( $request_method == "POST") 
     {
-        $username = $request_data[ "username"];
-        $title = $request_data[ "title"];
-        $description = $request_data[ "description"];
-        $content = $request_data[ "content"];
-        $tags = $request_data[ "tags"];
+        $required_keys_thread_creation = ["username", "title", "description", "content", "tags"];
+        $required_keys_one_specific_thread = ["username", "timestamp", "thread_id"];
 
-        date_default_timezone_set( "Europe/Stockholm");
-        $timestamp = [ "date" => date( "d-m-Y"), "time" => date( "H:i:s")];
-
-        foreach( $users as $user) 
+        if( count( array_intersect( $required_keys_thread_creation, array_keys( $request_data))) === count( $required_keys_thread_creation)) 
         {
-            if( $user[ "username"] == $request_data[ "username"]) 
+            $username = $request_data[ "username"];
+            $title = $request_data[ "title"];
+            $description = $request_data[ "description"];
+            $content = $request_data[ "content"];
+            $tags = $request_data[ "tags"];
+    
+            date_default_timezone_set( "Europe/Stockholm");
+            $timestamp = [ "date" => date( "d-m-Y"), "time" => date( "H:i:s")];
+    
+            foreach( $users as $user) 
             {
-                $username_id = $user[ "id"];
-                $user_img = $user[ "img_name"];
+                if( $user[ "username"] == $request_data[ "username"]) 
+                {
+                    $username_id = $user[ "id"];
+                    $user_img = $user[ "img_name"];
+                }
+            }
+    
+            $highest_id = 0;
+            foreach( $threads as $thread) 
+            {
+                if( $thread[ "thread_id"] > $highest_id)
+                {
+                    $highest_id = $thread[ "thread_id"];
+                }
+            }
+            $next_id = $highest_id + 1;
+    
+            $thread = [
+                "username" => $username,
+                "username_id" => $username_id,
+                "thread_id" => $next_id,
+                "title" => $title,
+                "description" => $description,
+                "content" => $content,
+                "timestamp" => $timestamp,
+                "views" => 0,
+                "comments" => [],
+                "tags" => $tags,
+                "img_name" => $user_img
+            ];
+    
+            $threads[] = $thread;
+            $json = json_encode( $threads, JSON_PRETTY_PRINT);
+            file_put_contents( $threads_file, $json);
+            send_JSON( $thread);
+        }
+        else if( count( array_intersect( $required_keys_one_specific_thread, array_keys( $request_data))) === count( $required_keys_one_specific_thread))
+        {
+            foreach( $threads as $index => $thread) 
+            {
+        
+                if( $thread[ "thread_id"] == $request_data[ "thread_id"]) 
+                {
+                    send_JSON($thread);
+                }
             }
         }
-
-        $highest_id = 0;
-        foreach( $threads as $thread) 
+        else 
         {
-            if( $thread[ "thread_id"] > $highest_id)
-            {
-                $highest_id = $thread[ "thread_id"];
-            }
+            $message = ["message" => "Error in POST body. Wrong keys used. Read API dokumentation"];
+            send_JSON($message);
         }
-        $next_id = $highest_id + 1;
-
-        $thread = [
-            "username" => $username,
-            "username_id" => $username_id,
-            "thread_id" => $next_id,
-            "title" => $title,
-            "description" => $description,
-            "content" => $content,
-            "timestamp" => $timestamp,
-            "views" => 0,
-            "comments" => [],
-            "tags" => $tags,
-            "img_name" => $user_img
-        ];
-
-        $threads[] = $thread;
-        $json = json_encode( $threads, JSON_PRETTY_PRINT);
-        file_put_contents( $threads_file, $json);
-        send_JSON( $thread);
     }
 ?>
