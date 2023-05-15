68404708876bc9768f4cfc1cdb63af0b13b76003
Adam Eliasson <adam.eliasson.swe@gmail.com>
2023-05-15 14:32:21 +0200

    converted the php to javasqript

--- JS/account.js
+++ JS/account.js
@@ -1,128 +1,117 @@
+async function renderAccountPage () {
 
-const searchQuery = window.location.search;
-    const urlParams = new URLSearchParams( searchQuery);
-    const userPageName = urlParams.get( "un");
-
-    if(current_user === userPageName){
-      document.querySelector(".imgEditContainer").innerHTML += `
-      <form action="../API/register.php" id="formUpload" method="PATCH" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <button type="submit">Upload</button>
-      </form>
-      `
-    }
-
-    if(current_user === userPageName){
-      document.querySelector(".imgEditContainer").innerHTML += `
-      <button id='editButton'> Edit profile </button>
-      `
-    }
-
-const modal = document.querySelector("#accountDialog");
-
-const profileButton = document.querySelector("#editButton");
-editebleDivs = Array.from(document.querySelectorAll(".editableDivs-accountPage"));
-function editProfile () {
-  profileButton.textContent = "Save";
-  profileButton.removeEventListener("click", editProfile);
-  profileButton.addEventListener("click", saveProfileEdits);
-    editebleDivs.forEach(element => {
-      element.setAttribute("contenteditable", true);
-    });
-    editebleDivs[0].focus();
-}
-
-function saveProfileEdits () {
-  profileButton.textContent = "Edit profile";
-  profileButton.removeEventListener("click", saveProfileEdits);
-  profileButton.addEventListener("click", editProfile);
-  editebleDivs.forEach(element => {
-    element.setAttribute("contenteditable", false);
-  });
-  const userPatchRequest = new Request("../../API/account.php", {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      user: current_user,
-      profile_info: {
-        fullname: editebleDivs[0].textContent,
-        discord: editebleDivs[1].textContent,
-        bio: editebleDivs[2].textContent,
-      },
-    }),
-});
-
-fetchFunction(userPatchRequest);
-
-}
-profileButton.addEventListener("click", editProfile);
-
-const form_dom = document.getElementById("formUpload");
-form_dom.addEventListener("submit", async function (event) {
-
-  event.preventDefault();
-
-  const formData = new FormData(form_dom);
-  formData.append("username", JSON.parse(window.localStorage.getItem("user")));
-  const profileImgRequest = new Request("../../API/account.php", {
-      method: "POST",
-      body: formData,
-  });
-
-  let patchToImage = await fetchFunction(profileImgRequest);
-
-  if(patchToImage.resource["message"]){
-    document.querySelector(".imgMessage").textContent = patchToImage.resource.message;
+  const userInfoRequest = new Request( "../../API/thread.php?un=" + current_user);
+  let userObjekt = await fetchFunction( userInfoRequest);
+  // resourse {
+  //   threads: [],
+  //   profileInfo: {
+  //     img_name: #,
+  //     fullname: #,
+  //     discord: #,
+  //     bio: #
+  //   }
+  // }
+  if(userObjekt.response.ok){
+    loadThreads( userObjekt.resource.threads, "You have no threads, Post a thread here...");
+  }else {
+    loadThreads( [], "Something went wrong... Please try again");
   }
-  
-  setTimeout( () => {
-    location.reload();
-  }, 1000);  
-})
+
+  const profileInfo = userObjekt.resource.profile_info
+  document.querySelector( ".profilSettings-accountPage").innerHTML = `
+    <div class="imgEditContainer"> 
+    <div class='profilePicture-accountPage'><img src=${serverEndpoint}/API/PROFILE_IMG/${profileInfo.img_name}></div>
+    <p class="imgMessage"></p> 
+    </div>
+    <div class='userInfo'>
+        <div class='infoParent'>
+            <div class='info editableDivs-accountPage' contenteditable='false'>${profileInfo.fullname}</div>
+            <div class='info editableDivs-accountPage' contenteditable='false'>${profileInfo.discord}</div>
+        </div>
+        <div class='profileBio editableDivs-accountPage' contenteditable='false'>${profileInfo.bio}</div>
+    </div>
+    </div> 
+  `;
+
+  const searchQuery = window.location.search;
+  const urlParams = new URLSearchParams( searchQuery);
+  const userPageName = urlParams.get( "un");
+
+  if(current_user === userPageName){
+    document.querySelector(".imgEditContainer").innerHTML += `
+    <form action="../API/register.php" id="formUpload" method="PATCH" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit">Upload</button>
+    </form>
+    <button id='editButton'> Edit profile </button>
+    `
+  }
 
 
-// let form = document.querySelector("form");
-// form.addEventListener("submit",  function (event) {
-//     event.preventDefault();
-//     modal.close();
-//     const values = form.querySelectorAll("input");
-//     const bioText = form.querySelector("textarea").value;
-//     console.log(bioText);
-//     document.querySelector(".userInfo").innerHTML = `
-//         <div class="infoParent">
-//             <div class="info">${values[0].value}</div>
-//             <div class="info">${values[1].value}</div>
-//         </div>
-//         <div class='profileBio'>${bioText}</div>
-//     `;
+  const profileButton = document.querySelector("#editButton");
+  editebleDivs = Array.from(document.querySelectorAll(".editableDivs-accountPage"));
+  function editProfile () {
+    profileButton.textContent = "Save";
+    profileButton.removeEventListener("click", editProfile);
+    profileButton.addEventListener("click", saveProfileEdits);
+      editebleDivs.forEach(element => {
+        element.setAttribute("contenteditable", true);
+      });
+      editebleDivs[0].focus();
+  }
 
-//     const userPatchRequest = new Request("../../API/account.php", {
-//         method: "PATCH",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({
-//           user: current_user,
-//           profile_info: {
-//             fullname: values[0].value,
-//             discord: values[1].value,
-//             bio: bioText,
-//           },
-//         }),
-//     });
+  function saveProfileEdits () {
+    profileButton.textContent = "Edit profile";
+    profileButton.removeEventListener("click", saveProfileEdits);
+    profileButton.addEventListener("click", editProfile);
+    editebleDivs.forEach(element => {
+      element.setAttribute("contenteditable", false);
+    });
 
-//     fetchFunction(userPatchRequest);
-            
-// })
+    const userPatchRequest = new Request("../../API/account.php", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: current_user,
+        profile_info: {
+          fullname: editebleDivs[0].textContent,
+          discord: editebleDivs[1].textContent,
+          bio: editebleDivs[2].textContent,
+        },
+      }),
+  });
 
-// modal.addEventListener("click", e => {
-//     const dialogDimensions = modal.getBoundingClientRect()
-//     if (
-//       e.clientX < dialogDimensions.left ||
-//       e.clientX > dialogDimensions.right ||
-//       e.clientY < dialogDimensions.top ||
-//       e.clientY > dialogDimensions.bottom
-//     ) {
-//       modal.close()
-//     }
-//   })
+  fetchFunction(userPatchRequest);
+}
+  profileButton.addEventListener("click", editProfile);
+
+
+  // Here we manage the users Profile Image
+  const form_dom = document.getElementById("formUpload");
+  form_dom.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+
+    const formData = new FormData(form_dom);
+    formData.append("username", JSON.parse(window.localStorage.getItem("user")));
+    const profileImgRequest = new Request("../../API/account.php", {
+        method: "POST",
+        body: formData,
+    });
+
+    let patchToImage = await fetchFunction(profileImgRequest);
+
+    if(patchToImage.resource["message"]){
+      document.querySelector(".imgMessage").textContent = patchToImage.resource.message;
+    }
+    
+    setTimeout( () => {
+      location.reload();
+    }, 1000);  
+  })
+
+}
+
+renderAccountPage();
 
 
