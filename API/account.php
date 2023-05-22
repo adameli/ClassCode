1691455860cf513530f07f12a2e4dddb9a4db0d4
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-05-22 20:41:40 +0100

    small thing

--- API/account.php
+++ API/account.php
@@ -5,7 +5,7 @@
     if( $request_method == "POST") 
     {
         //CHECKS FILE SIZE, MAX SIZE: 50 kb (50 000 Bytes)
-        if( $_FILES[ "file"][ "size"] > 50000 || $_FILES[ "file"][ "size"] == 0) 
+        if( $_FILES[ "file"][ "size"] >= 50000 || $_FILES[ "file"][ "size"] == 0) 
         {
             $message = [ "message" => "Image-size too big, max-limit is 50 Kb."];
             send_JSON( $message, 403);
